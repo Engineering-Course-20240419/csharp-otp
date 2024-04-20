884ef016b3ab8c57be473761dbbd9d0c8d63ca11
unknown <joseph@odd-e.com>
2024-04-20 16:11:06 +0800

    :sparkles: no budget

---
+++ NUnit.Tests/NewBudgetTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using csharp_otp_2019;
+
+namespace NUnit.Tests
+{
+    [TestFixture]
+    class NewBudgetTest
+    {
+        [Test]
+        public void NoBudget()
+        {
+            var queryBudget = new QueryBudget();
+            
+            var actual = queryBudget.query(new DateTime(2024, 4, 20), new DateTime(2024, 4, 21));
+
+            Assert.AreEqual(0, actual);
+        }
+    }
+}
---
+++ csharp-otp/QueryBudget.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_otp_2019
+{
+    public class QueryBudget
+    {
+        public int query(DateTime start, DateTime end)
+        {
+            return 0;
+        }
+    }
+}
