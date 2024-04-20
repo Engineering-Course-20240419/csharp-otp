b0323e266a960e27ecd20a739b590a39024f54d6
unknown <joseph@odd-e.com>
2024-04-20 16:28:14 +0800

    :sparkles: start after budget start

--- NUnit.Tests/NewBudgetTest.cs
+++ NUnit.Tests/NewBudgetTest.cs
@@ -4,21 +4,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using csharp_otp;
 using csharp_otp_2019;
+using Moq;
 
 namespace NUnit.Tests
 {
     [TestFixture]
     class NewBudgetTest
     {
+        Mock<IBudgetRepo> stubRepo;
+        QueryBudget queryBudget;
+
+        [SetUp]
+        public void setup()
+        {
+            stubRepo = new Mock<IBudgetRepo>();
+            queryBudget = new QueryBudget(stubRepo.Object);
+        }
+
         [Test]
         public void NoBudget()
         {
-            var queryBudget = new QueryBudget();
-            
+            GivenBudgets();
+
             var actual = queryBudget.query(new DateTime(2024, 4, 20), new DateTime(2024, 4, 21));
 
             Assert.AreEqual(0, actual);
+        }
+
+        private void GivenBudgets(params Budget[] budgets)
+        {
+            stubRepo.Setup(r => r.GetAll()).Returns(budgets.ToList());
+        }
+
+        [Test]
+        public void xxx()
+        {
+            GivenBudgets(new Budget
+            {
+                YearMonth = "202404",
+                Amount = 30
+            });
+            
+            var actual = queryBudget.query(new DateTime(2024, 4, 20), new DateTime(2024, 4, 20));
+
+            Assert.AreEqual(1, actual);
         }
     }
 }
--- csharp-otp/QueryBudget.cs
+++ csharp-otp/QueryBudget.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using csharp_otp;
 
 namespace csharp_otp_2019
 {
     public class QueryBudget
     {
+        private readonly IBudgetRepo _budgetRepo;
+
+        public QueryBudget(IBudgetRepo budgetRepo)
+        {
+            _budgetRepo = budgetRepo;
+        }
+
         public int query(DateTime start, DateTime end)
         {
-            return 0;
+            if (_budgetRepo.GetAll().Count == 0)
+            {
+                return 0;
+            }
+            return 1;
         }
     }
 }
