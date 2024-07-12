71f2f8936a58b607658c23b4bd3cb17b8158a20a
unknown <joseph.yao.ruozhou@gmail.com>
2024-07-12 11:38:38 +0800

    :sparkles: add a sample test with ms fakes

---
+++ MsFakes/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MsFakes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MsFakes")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d00f6b32-f3c8-4320-a8ba-7178b97cee93")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MsFakes/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using csharp_otp;
+using csharp_otp.Fakes;
+using Microsoft.QualityTools.Testing.Fakes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Runtime.Remoting.Messaging;
+
+namespace MsFakes
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GetIns_All_MsFakes()
+        {
+            
+            using (ShimsContext.Create())
+            {
+                ShimAuthenticationService.AllInstances.IsValidStringString = 
+                    (AuthenticationService instance, string username, string password) => { 
+                        return true; 
+                    };
+                ShimAuthenticationService.GetIns = () => new AuthenticationService();
+
+                var actual = AuthenticationService.GetIns().IsValid("abc", "123");
+
+                Assert.IsTrue(actual);
+            }
+        }
+    }
+}
